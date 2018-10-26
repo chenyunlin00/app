900cb247267151a21c98f68382cd283cbfcc7a3c
chenyunlin00 <u_u_u@126.com>
2018-10-26 11:45:22 +0800

    aa

--- application/controllers/Tool.php
+++ application/controllers/Tool.php
@@ -17,7 +17,15 @@ class Tool extends CI_Controller {
 
     public function calculate()
     {
-        $something = $this->input->post('func');
-        var_dump($something);
+        $a_lon = $this->input->post('a_lon');
+        $a_lat = $this->input->post('a_lat');
+        $b_lon = $this->input->post('b_lon');
+        $b_lat = $this->input->post('b_lat');
+        exec("./a.out 120 30 110 30", $ret);
+        $len_km = $ret[0];
+        $angle = $ret[1];
+        echo json_encode(['code'=>0, 'msg'=>'',
+            'data'=>['len_km'=>$len_km, 'angle'=>$angle]
+            ]);
     }
 }
--- application/views/gps.php
+++ application/views/gps.php
@@ -1,14 +1,43 @@
 <script type="text/javascript">
 function calculate()
 {
-    if ($("#a_lat").val() == "")
+    var a_lat = 0;
+    var a_lon = 0;
+    var b_lat = 0;
+    var b_lon = 0;
+    if ($("#a_lat").val() != "")
     {
-        alert("A点经度为空");
+        a_lat = parseFloat($("#a_lat").val());
     }
-    $.post("calculate", { "a_lat": $("#a_lat").val() },
+    if ($("#a_lon").val() != "")
+    {
+        a_lon = parseFloat($("#a_lon").val());
+    }
+    if ($("#b_lat").val() != "")
+    {
+        b_lat = parseFloat($("#b_lat").val());
+    }
+    if ($("#b_lon").val() != "")
+    {
+        b_lon = parseFloat($("#b_lon").val());
+    }
+    $.post("calculate", 
+            { 
+                "a_lat" : a_lat,
+                "a_lon" : a_lon,
+                "b_lat" : b_lat,
+                "b_lon" : b_lon
+            },
             function(data){
-            alert(data.len_km); // John
-            }, "json");
+                if (data.code != 0) {   
+                    alert(data.msg);
+                    return;
+                }
+                $("#len_km").val(data.data.len_km);
+                $("#angle").val(data.data.angle);
+            },
+            "json"
+            );
 }
 </script>
 
@@ -24,17 +53,17 @@ function calculate()
 <br>
 <div class="input-group">
 <span class="input-group-addon">A点纬度</span>
-<input type="number" class="form-control" placeholder="0" >
+<input type="number" class="form-control" placeholder="0" id="a_lon">
 </div>
 <br>
 <div class="input-group">
 <span class="input-group-addon">B点经度</span>
-<input type="text" class="form-control" placeholder="0">
+<input type="number" class="form-control" placeholder="0" id="b_lat">
 </div>
 <br>
 <div class="input-group">
 <span class="input-group-addon">B点纬度</span>
-<input type="text" class="form-control" placeholder="0" >
+<input type="number" class="form-control" placeholder="0" id="b_lon">
 </div>
 
 </div>
@@ -51,7 +80,7 @@ function calculate()
 </div>
 <div class="input-group">
 <span class="input-group-addon">两点距离</span>
-<input type="text" readonly class="form-control" placeholder="" value="<?php echo $len_km;?>">
+<input type="text" readonly class="form-control" placeholder="" id="len_km">
 <span class="input-group-addon">公里</span>
 </div>
 <div class="input-group">
@@ -66,7 +95,7 @@ function calculate()
 </div>
 <div class="input-group">
 <span class="input-group-addon">B相对A方位</span>
-<input type="text" readonly class="form-control" placeholder="" value="<?php echo $len_km;?>">
+<input type="text" readonly class="form-control" placeholder="" id="angle">
 <span class="input-group-addon">度</span>
 </div>
 <div class="input-group">
